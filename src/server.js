e41f20edffc6c518e10ea72ae9ba0cc027a08c02
Melone155 <101599412+Melone155@users.noreply.github.com>
2025-06-01 00:26:54 +0200

    .

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import cors from 'cors';
+import express from "express";
+import { connectDB } from "./backend/dbconnection.js";
+import loginRouter from './backend/login.js';
+
+const app = express();
+const PORT = 5000;
+
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST','DELETE', 'PATCH', 'PUT'],
+    allowedHeaders: '*',
+};
+
+app.use('/payment/webhook', express.raw({ type: 'application/json' }));
+
+app.use(cors(corsOptions))
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server läuft auf http://localhost:${PORT}`);
+});
+
+await connectDB();
+
+app.use('/login', loginRouter);
+
+
